1d4f7e46236248b10aa61eb3d5b6280ae71bfe06
Choffel <daniilmiadzve@gmail.com>
2026-05-30 21:56:54 +0300

    feat:add background worker

---
+++ src/OrbitTracker.Core/BackgroundService/SatteliteTrackerWorker.cs
@@ -0,0 +1,37 @@
+﻿using OrbitTracker.Core.Contracts;
+
+namespace OrbitTracker.Core.BackgroundService;
+
+public class SatteliteTrackerWorker : Microsoft.Extensions.Hosting.BackgroundService
+{
+    private readonly ISatelliteRepository _satelliteRepository;
+    private readonly ISatelliteTrackingService _trackerService;
+
+    public SatteliteTrackerWorker(ISatelliteTrackingService trackerService, ISatelliteRepository satelliteRepository)
+    {
+        _satelliteRepository = satelliteRepository;
+        _trackerService = trackerService;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                var currentPosition = await _trackerService.TrackSatelliteAsync(stoppingToken);
+                await _satelliteRepository.SavePositionAsync(currentPosition);
+            }
+            catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+            {
+                break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+}
