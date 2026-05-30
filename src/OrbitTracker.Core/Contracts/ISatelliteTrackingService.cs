d82d1bb8682e02aa723e94412c64c70f8ebe3318
Choffel <daniilmiadzve@gmail.com>
2026-05-30 21:57:54 +0300

    feat:add satellite service for getting api data

---
+++ src/OrbitTracker.Core/Contracts/ISatelliteTrackingService.cs
@@ -0,0 +1,9 @@
+﻿using OrbitTracker.Core.Models;
+using OrbitTracker.Infrastructure.DtosForService;
+
+namespace OrbitTracker.Core.Contracts;
+
+public interface ISatelliteTrackingService
+{
+    Task<SatellitePosition> TrackSatelliteAsync(CancellationToken cancellationToken);
+}
---
+++ src/OrbitTracker.Core/Service/SatelliteTrackerService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using OrbitTracker.Core.Contracts;
+using OrbitTracker.Core.Models;
+using OrbitTracker.Infrastructure.DtosForService;
+
+namespace OrbitTracker.Core.Service;
+
+public class SatelliteTrackerService : ISatelliteTrackingService
+{
+    private string url = "http://api.open-notify.org/iss-now.json";
+    private readonly HttpClient client;
+    
+    
+    public async Task<SatellitePosition> TrackSatelliteAsync(CancellationToken cancellationToken)
+    {
+        var response = await client.GetFromJsonAsync<IssResponse>(url, cancellationToken);
+
+        if (response == null)
+        {
+            throw new Exception("Failed to retrieve satellite position.");
+        }
+    
+        return new SatellitePosition
+        {
+            Latitude = response.Latitude,
+            Longitude = response.Longitude,
+            TimeStamp = DateTimeOffset.FromUnixTimeSeconds(response.Timestamp).DateTime
+        };
+    }
+}
