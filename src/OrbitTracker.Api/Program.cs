b0f0364f219de4eb9dfdfe04e2000d023848e5f1
Choffel <daniilmiadzve@gmail.com>
2026-05-31 02:27:55 +0300

    feat: add postgres sql connection

--- src/OrbitTracker.Api/Program.cs
+++ src/OrbitTracker.Api/Program.cs
@@ -1,10 +1,24 @@
+using OrbitTracker.Core.BackgroundService;
+using OrbitTracker.Core.Contracts;
+using OrbitTracker.Infrastructure.Data;
+using OrbitTracker.Infrastructure.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add PostgreSQL DbContext
+builder.Services.AddDbContext<OrbitTrackerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Repository
+builder.Services.AddScoped<ISatelliteRepository, SatelliteRepository>();
+
 var app = builder.Build();
+
+builder.Services.AddHostedService<SatteliteTrackerWorker>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ src/OrbitTracker.Infrastructure/Data/OrbitTrackerDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrbitTracker.Core.Models;
+
+namespace OrbitTracker.Infrastructure.Data;
+
+public class OrbitTrackerDbContext : DbContext
+{
+    public DbSet<SatellitePosition> SatellitePositions { get; set; } = null!;
+
+    public OrbitTrackerDbContext(DbContextOptions<OrbitTrackerDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<SatellitePosition>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            
+            entity.Property(e => e.Latitude)
+                .HasPrecision(10, 8);
+            
+            entity.Property(e => e.Longitude)
+                .HasPrecision(11, 8);
+            
+            entity.Property(e => e.TimeStamp)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            entity.ToTable("satellite_positions");
+        });
+    }
+}
+
