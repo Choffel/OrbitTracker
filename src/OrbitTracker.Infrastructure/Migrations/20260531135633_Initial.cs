89bde2b05f2be8e78bd7f47d04276d8c757390ff
Choffel <daniilmiadzve@gmail.com>
2026-06-01 19:25:45 +0300

    feat(database): add initial ef core migration for satellite positions

---
+++ src/OrbitTracker.Infrastructure/Migrations/20260531135633_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrbitTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "satellite_positions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", precision: 10, scale: 8, nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", precision: 11, scale: 8, nullable: false),
+                    TimeStamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_satellite_positions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "satellite_positions");
+        }
+    }
+}
