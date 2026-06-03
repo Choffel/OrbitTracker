2dfef45b41e7c0702a81fa219665c5d0a8c50c10
Choffel <daniilmiadzve@gmail.com>
2026-06-04 01:24:50 +0300

    feat(satellite): implement and register satellite caching in infrastructure

--- src/OrbitTracker.Api/Program.cs
+++ src/OrbitTracker.Api/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using OrbitTracker.Building_Blocks.Redis.Contract;
+using OrbitTracker.Building_Blocks.Redis.Service;
 using OrbitTracker.Core.BackgroundService;
 using OrbitTracker.Core.Contracts;
 using OrbitTracker.Core.Service;
+using OrbitTracker.Infrastructure.Caching;
 using OrbitTracker.Infrastructure.Data;
 using OrbitTracker.Infrastructure.Repository;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +27,12 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<OrbitTrackerDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(serviceProvider => ConnectionMultiplexer.Connect("localhost"));
+
+builder.Services.AddScoped<ICacheService, CacheService>();
+builder.Services.AddScoped<ISatelliteCacheService, RedisSateliteCacheService>();
 
 builder.Services.AddScoped<ISatelliteRepository, SatelliteRepository>();
 
---
+++ src/OrbitTracker.Infrastructure/Caching/RedisSateliteCacheService.cs
@@ -0,0 +1,28 @@
+﻿using OrbitTracker.Building_Blocks.Redis.Contract;
+using OrbitTracker.Core.Contracts;
+using OrbitTracker.Core.Models;
+
+namespace OrbitTracker.Infrastructure.Caching;
+
+public class RedisSateliteCacheService : ISatelliteCacheService
+{
+    private readonly ICacheService _cacheService;
+    
+    private const string CacheKey = "satellite:iss:current";
+    
+    public RedisSateliteCacheService(ICacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+    
+    public async Task CachePositionAsync(SatellitePosition position, CancellationToken cancellationToken)
+    {
+        await _cacheService.SetAsync(CacheKey, position, cancellationToken: cancellationToken);
+    }
+
+    
+    public async Task<SatellitePosition?> GetCachedPositionAsync(CancellationToken cancellationToken)
+    {
+        return await _cacheService.GetAsync<SatellitePosition>(CacheKey, cancellationToken);
+    }
+}
