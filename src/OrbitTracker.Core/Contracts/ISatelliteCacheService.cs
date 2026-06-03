140f8909ca5097e2c919d62a60a6df86d465aa42
Choffel <daniilmiadzve@gmail.com>
2026-06-04 01:24:45 +0300

    feat(satellite): add ISatelliteCacheService interface to Core

---
+++ src/OrbitTracker.Core/Contracts/ISatelliteCacheService.cs
@@ -0,0 +1,10 @@
+﻿using OrbitTracker.Core.Models;
+
+namespace OrbitTracker.Core.Contracts;
+
+public interface ISatelliteCacheService
+{
+    Task<SatellitePosition?> GetCachedPositionAsync(CancellationToken cancellationToken);
+    
+    Task CachePositionAsync(SatellitePosition position, CancellationToken cancellationToken);
+}
